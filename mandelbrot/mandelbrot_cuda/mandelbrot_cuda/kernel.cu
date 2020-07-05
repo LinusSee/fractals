c98df604c139023ac1559fddcadc09305d754435
Linus <linussee@web.de>
2020-07-05 17:06:35 +0200

    feat: fixed exception, description will follow

--- mandelbrot/mandelbrot_cuda/mandelbrot_cuda/kernel.cu
+++ mandelbrot/mandelbrot_cuda/mandelbrot_cuda/kernel.cu
@@ -67,7 +67,75 @@ void mandelbrot_point(int* point, complex<double> c, int max_iterations)
 __global__
 void mandelbrot_set(int* m_set, double start_x, double end_x, double start_y, double end_y, int num_points, int max_iterations)
 {
-    double spacing_x = abs(end_x - start_x) / ((double) num_points - 1.0);
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    int length = num_points * num_points;
+    
+    double spacing_x = abs(end_x - start_x) / ((double)num_points - 1.0);
+    double spacing_y = abs(end_y - start_y) / ((double)num_points - 1.0);
+
+    for (int i = index; i < length; i += stride)
+    {
+        int pos_x = i % num_points;
+        int pos_y = i / num_points;
+        double x = start_x + pos_x * spacing_x;
+        double y = start_y + pos_y * spacing_y;
+
+        thrust::complex<double> c = thrust::complex<double>(x, y);
+
+        int iterations = max_iterations;
+        thrust::complex<double> z = thrust::complex<double>(0, 0);
+        for (int i = 0; i < max_iterations; i++)
+        {
+            z = z * z + c;
+            if (thrust::abs(z) > 4)
+            {
+                iterations = i;
+                break;
+            }
+        }
+        m_set[i] = iterations;
+    }
+
+
+    
+    /*
+    double spacing_x = abs(end_x - start_x) / ((double)num_points - 1.0);
+    double spacing_y = abs(end_y - start_y) / ((double)num_points - 1.0);
+
+    double current_x = start_x;
+    double current_y = start_y;
+
+    for (int i = 0; i < num_points * num_points; i++)
+    {
+        int index = i * num_points + j;
+
+        thrust::complex<double> c = thrust::complex<double>(current_x, current_y);
+
+        int iterations = max_iterations;
+        thrust::complex<double> z = thrust::complex<double>(0, 0);
+        for (int i = 0; i < max_iterations; i++)
+        {
+            z = z * z + c;
+
+            if (thrust::abs(z) > 4)
+            {
+                iterations = i;
+                break;
+            }
+        }
+
+        //int iterations = mandelbrot_point(c, max_iterations);
+
+        m_set[index] = iterations;
+        current_x = current_x + spacing_x;
+    current_x = start_x;
+    current_y = current_y + spacing_y;
+    }*/
+}
+
+/*
+double spacing_x = abs(end_x - start_x) / ((double) num_points - 1.0);
     double spacing_y = abs(end_y - start_y) / ((double) num_points - 1.0);
 
     double current_x = start_x;
@@ -102,7 +170,7 @@ void mandelbrot_set(int* m_set, double start_x, double end_x, double start_y, do
         current_x = start_x;
         current_y = current_y + spacing_y;
     }
-}
+    */
 
 int* point_color(int iterations, int max_iterations)
 {
@@ -158,19 +226,21 @@ sf::Uint8* set_to_image(int* m_set, int num_points, int max_iterations)
 
 int main()
 {
-    constexpr int num_points = 300;
+    constexpr int num_points = 1000;
     constexpr int max_iterations = 120;
     size_t size_old = num_points * num_points;
     size_t size = num_points * num_points * sizeof(int);
 
 
-    //int blockSize = 256;
-    //int numBlocks = (num_points + blockSize - 1) / blockSize;
+    int blockSize = 256;
+    int numBlocks = (num_points + blockSize - 1) / blockSize;
+    //blockSize = 1;
+    //numBlocks = 1;
     //int* m_set = new int[size_old];
     int* m_set;
     //int* mapped = (int*)malloc(size);
     cudaMallocManaged(&m_set, size);
-    mandelbrot_set<<<1, 1>>>(m_set, -2.25, 0.75, -1.5, 1.5, num_points, max_iterations);
+    mandelbrot_set<<<numBlocks, blockSize>>>(m_set, -2.25, 0.75, -1.5, 1.5, num_points, max_iterations);
     //mandelbrot_set(m_set, -2.25, 0.75, -1.5, 1.5, num_points, max_iterations);
     cudaDeviceSynchronize();
 
